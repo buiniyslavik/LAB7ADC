6a0ea7f80c3ab486981539c5e7f25c580a1c094a
svosin <svosin@airmail.cc>
2019-11-15 14:18:37 +0300

    sts/out fix

--- LAB7ADC/main.asm
+++ LAB7ADC/main.asm
@@ -6,11 +6,27 @@
   ldi r16,high(RAMEND)  
   out sph,r16 
 
+;UART things 
   ; allow interrupts
   ldi r16, 0x0f 
   sts EICRA, r16 
   ldi r16, 0x03 
   out EIMSK, r16 
+
+  ; set speed
+  .equ CLK=16000000 
+  .equ BAUD=9600 
+  .equ UBRR0_value = (CLK/(BAUD*16)) - 1 
+  ldi r16, high(UBRR0_value) 
+  sts UBRR0H, r16 
+  ldi r16, low(UBRR0_value) 
+  sts UBRR0L, r16
+
+  ; turn on UART
+  ldi r16, (1<<TXEN0)|(1<<RXEN0)|(1<<RXCIE0)           
+  sts UCSR0B,R16 
+  ldi r16,(1<< UCSZ00)|(1<< UCSZ01)
+; end of UART init
 
   ; enable internal Vref
   ldi r16,0x40 
@@ -18,7 +34,7 @@
 
   ; set portB OUT
   ldi r16,0xFF 
-  sts DDRB,r16 
+  out DDRB,r16 
 
 main: 
   ; read compare result
