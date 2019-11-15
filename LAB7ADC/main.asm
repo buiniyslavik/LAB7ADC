2f226160e9723b79a15652f36579461f43306d9a
svosin <svosin@airmail.cc>
2019-11-15 14:39:43 +0300

    Task 1, attempt 2

--- LAB7ADC/main.asm
+++ LAB7ADC/main.asm
@@ -1,5 +1,10 @@
 .include "m328Pdef.inc"  
-
+.equ CLK=16000000  
+.equ BAUD=9600 
+.equ UBRR0_value = (CLK/(BAUD*16)) - 1 
+.org 0  
+  jmp Reset 
+  Reset:
   ; stack
   ldi r16,low(RAMEND) 
   out spl,r16 
@@ -7,25 +12,19 @@
   out sph,r16 
 
 ;UART things 
-  ; allow interrupts
-  ldi r16, 0x0f 
-  sts EICRA, r16 
-  ldi r16, 0x03 
-  out EIMSK, r16 
-
-  ; set speed
-  .equ CLK=16000000 
-  .equ BAUD=9600 
-  .equ UBRR0_value = (CLK/(BAUD*16)) - 1 
-  ldi r16, high(UBRR0_value) 
-  sts UBRR0H, r16 
-  ldi r16, low(UBRR0_value) 
-  sts UBRR0L, r16
-
-  ; turn on UART
-  ldi r16, (1<<TXEN0)|(1<<RXEN0)|(1<<RXCIE0)           
-  sts UCSR0B,R16 
-  ldi r16,(1<< UCSZ00)|(1<< UCSZ01)
+;установка частоты приёмо/передачи 
+  ldi r16, high(UBRR0_value)  
+  sts UBRR0H, r16  
+  ldi r16, low(UBRR0_value)  
+  sts UBRR0L, r16  
+;разрешение передачи 
+  ldi r16,(1<<TXEN0)  
+  sts UCSR0B,R16  
+;длина слова 8 бит 
+  ldi r16,(1<< UCSZ00)|(1<< UCSZ01)  
+  sts UCSR0C,R16  
+;разрешение всех прерываний 
+  sei 
 ; end of UART init
 
   ; enable internal Vref
@@ -36,27 +35,62 @@
   ldi r16,0xFF 
   out DDRB,r16 
 
-main: 
-  ; read compare result
+VoltOK: 
   in r17,ACSR 
   ; if 5th bit in R17 is set, don't jump
   sbrs r17,5 
-	jmp voltlev 
-
-  ; turn the LED off
+	jmp voltlev
   ldi r16,0x00 
   out PORTB,r16 
-  jmp main  
+;PASTED
+ ldi zl,low(NorV<<1);память адресуется словами адрес старшие 15 бит, 
+;а младший бит - определяет часть слова 
+  ldi zh,high(NorV<<1);0 - младший байт, 1 - старший 
+  ldi r18,0x07;количество байт в сообщении 
+send: 
+  lpm r16, Z+ ;чтение слово из таблицы из ПЗУ 
+  sts UDR0,r16 ;отправка по uart 
+  subi r18,0x01 ;вычитание 1 из счетчика букв 
+repeat: 
+  lds r17, UCSR0A;чтение содержание регистра 
+  bst r17, 5;запись 5 бита (udre) регистра (ucsr0a) в бит t регистра sreg   
+  brtc repeat ;если t=0 
+  cpi r18,0x00 ;r18=0? 
+  breq   VoltOK ;если да, то повторяем сообщение 
+  jmp send ;если нет, то  следующий символ ;PASTED
+  ; turn the LED off
 
   ; LED ON state
 voltlev: 
   ; turn on the LED
   ldi r16,0xFF 
   out PORTB,r16 
-
   ; read the result
   in r17,ACSR 
   ; if 5th bit is clear, don't return and stay in voltlev
   sbrc r17,5 
 	ret 
+;PASTED
+ ldi zl,low(LowV<<1);память адресуется словами адрес старшие 15 бит, 
+;а младший бит - определяет часть слова 
+  ldi zh,high(LowV<<1);0 - младший байт, 1 - старший 
+  ldi r18,0x07;количество байт в сообщении 
+sendLow: 
+  lpm r16, Z+ ;чтение слово из таблицы из ПЗУ 
+  sts UDR0,r16 ;отправка по uart 
+  subi r18,0x01 ;вычитание 1 из счетчика букв 
+repeatLow: 
+  lds r17, UCSR0A;чтение содержание регистра 
+  bst r17, 5;запись 5 бита (udre) регистра (ucsr0a) в бит t регистра sreg   
+  brtc repeat ;если t=0 
+  cpi r18,0x00 ;r18=0? 
+  breq   voltlev ;если да, то повторяем сообщение 
+  jmp sendLow ;если нет, то  следующий символ 
+;PASTED
+
 jmp voltlev
+
+
+.cseg ;работа с ПЗУ 
+LowV: .db "Low Voltage",'\n' ;размещение сообщения в памяти ПЗУ 
+NorV: .db "Normal",'\n' ; размещение сообщения в памяти ПЗУ 
