664598dfc9fa21187d13f3165db9db7c68d7a9a3
svosin <svosin@airmail.cc>
2019-11-15 13:51:18 +0300

    first version

--- LAB7ADC/main.asm
+++ LAB7ADC/main.asm
@@ -1,12 +1,19 @@
-;
-; LAB7ADC.asm
-;
-; Created: 11/15/2019 1:46:42 PM
-; Author : home
-;
-
-
-; Replace with your application code
-start:
-    inc r16
-    rjmp start
+.include "m328Pdef.inc"  
+  ldi r16,0x40 
+  out ACSR,r16 
+  ldi r16,0xFF 
+  sts DDRB,r16 
+main: 
+  in r17,ACSR 
+  sbrs r17,5 
+  jmp voltlev 
+  ldi r16,0x00 
+  out PORTB,r16 
+  jmp main  
+voltlev: 
+  ldi r16,0xFF 
+  out PORTB,r16 
+  in r17,ACSR 
+  sbrc r17,5 
+  ret 
+jmp voltlev
